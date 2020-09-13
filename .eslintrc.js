a051b1bcc22ac8685bbb6d6e5cf59fc729991d1f
feerglas <info@feerglas.com>
2020-09-13 10:49:22 +0200

    fix: add webpack build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 const baseConfig = require('lyne-helper-eslint-config');
 
 baseConfig.globals = {
+  __dirname: 'readonly',
   module: 'readonly',
   require: 'readonly'
 };
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  mode: 'production',
+  output: {
+    filename: 'index.js',
+    library: 'lyneTriggerTravis',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [new CleanWebpackPlugin()],
+  target: 'node'
+};
